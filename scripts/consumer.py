f840d5d7db2fe3c254e9a17be496c7a381d720c1
madhuroopa <madhuroopa246@gmail.com>
2023-08-04 18:29:56 -0700

    added producer and consumer for kafka server

--- scripts/consumer.py
+++ scripts/consumer.py
@@ -1,3 +1,21 @@
+import pandas as pd
+import re
+import time
+import datetime
+from bs4 import BeautifulSoup
+import requests
+from kafka import KafkaConsumer,KafkaProducer
+import json
+from json import dumps,loads
+
+class Consumer:
+    def __init__(self):
+        self.topic='weekly-top-sellers'
+    def get_consumer(self):
+        print("Starting consumer...")
+        consumer = KafkaConsumer(self.topic, bootstrap_servers=['Mittu:9092'])
+        received_messages = []
+        try:
             for message in consumer:
                 json_data = message.value.decode('utf-8')  # Convert bytes to string
                 print(json_data)
