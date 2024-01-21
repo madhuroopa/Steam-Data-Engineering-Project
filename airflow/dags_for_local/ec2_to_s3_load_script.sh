e5bd3bcf0b98876e35af812430fed86f54da33a5
Madhuroopa <madhuroopa246@gmail.com>
2024-01-21 11:20:27 -0800

    Update ec2_to_s3_load_script.sh

--- airflow/dags_for_local/ec2_to_s3_load_script.sh
+++ airflow/dags_for_local/ec2_to_s3_load_script.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
+#  command -v aws checks whether the AWS CLI is installed by attempting to locate the executable in the system's PATH.
 if ! command -v aws &> /dev/null; then
     echo "AWS CLI is not installed. Please install and configure it."
     exit 1
 fi
 
-
+# checks whether it received the correct number of command-line arguments. If not (i.e., not equal to 2), 
+#it prints a usage message and exits with a status code of 1.
 if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <source-directory> <s3://bucket-name/destination>"
     exit 1
@@ -14,9 +15,10 @@ fi
 source_directory="$1"
 s3_destination="$2"
 
-# AWS CLI to copy data to S3 with the --recursive option
+# AWS CLI to copy data to S3 with the --recursive option, ensures that the entire directory structure is copied.
 aws s3 cp "$source_directory" "$s3_destination" --recursive
-
+#the script checks the exit status ($?). If the exit status is 0 (indicating success), it prints a success message.
+#Otherwise, it prints an error message and exits with a status code of 1.
 if [ $? -eq 0 ]; then
     echo "Data copied to S3 successfully."
 else
